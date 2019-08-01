59df543470c4292f7e53f9f8c85886608aff7a2f
PQ <qipanlml@gmail.com>
2019-08-01 16:31:40 +1000

    Try Base 1, np array and tensor conversion

--- Base_1.py
+++ Base_1.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 # import numpy as np
 
 v = torch.tensor([1,2,3,4,5,6])
@@ -10,3 +11,14 @@ f = torch.FloatTensor([1,2,3])
 print(f)
 print(f.dtype)
 print(f.size())
+
+print("===========================")
+a = np.array([1,2,3,4,5])
+tensor_cnv = torch.from_numpy(a)
+print("convert np array to torch tensor")
+print(tensor_cnv)
+print(tensor_cnv.type())
+
+print("convert torch tensor to np array")
+numpy_cnv = tensor_cnv.numpy()
+print(numpy_cnv)
