b2973603e7da94111f50b396447011a1bd7d866b
PQ <qipanlml@gmail.com>
2019-08-05 23:27:52 +1000

    image 2nd

--- Image_Recognise.py
+++ Image_Recognise.py
@@ -1,7 +1,7 @@
 import torch
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt 
-import numpy as numpy
+import numpy as np 
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
