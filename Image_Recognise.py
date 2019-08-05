8172f22345e37e7597de846963e9f0874fe9a327
PQ <qipanlml@gmail.com>
2019-08-05 22:13:30 +1000

    computer vision init

---
+++ Image_Recognise.py
@@ -0,0 +1,12 @@
+import torch
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt 
+import numpy as numpy
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+                                ])
+
+training_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+training_dataset
+
