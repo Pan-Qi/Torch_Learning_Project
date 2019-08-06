dddeb8d053012690f4f0266333bd79e9ab8f1d93
PQ <qipanlml@gmail.com>
2019-08-06 12:40:13 +1000

    Perceptron training & prediction is done

--- Perceptron.py
+++ Perceptron.py
@@ -6,10 +6,13 @@ from sklearn import datasets
 
 n_pts = 100
 centers = [[-0.5,0.5],[0.5,-0.5]]
-x,y = datasets.make_blobs(n_samples= n_pts, random_state=123,
-centers = centers, cluster_std = 0.4)
-x_data = torch.tensor(x)
-y_data = torch.tensor(y)
+x,y = datasets.make_blobs(n_samples= n_pts, random_state=123, centers = centers, cluster_std = 0.4)
+x_data = torch.Tensor(x)
+y_data = torch.Tensor(y.reshape(100,1))
+# print(x_data)
+# print(y_data)
+# print(type(x),type(y))
+# print(type(x_data),type(y_data))
 
 def scatter_plot():
     plt.scatter(x[y==0,0],x[y==0,1])
@@ -25,12 +28,18 @@ class Model(nn.Module):
     def forward(self, x):
         pred = torch.sigmoid(self.linear(x))
         return pred
+    def predict(self,x):
+        pred = self.forward(x)
+        if pred >= 0.5:
+            return 1
+        return 0
 
 torch.manual_seed(2)
 model = Model(2,1)
 
 [w,b] = model.parameters()
 w1,w2 = w.view(2)
+
 def get_params():
     return (w1.item(),w2.item(),b[0].item())
 
@@ -43,3 +52,28 @@ def plot_fit(title):
     scatter_plot()
 
 plot_fit('Init_Model')
+
+
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
+
+epochs = 1000
+losses = []
+for e in range(epochs):
+    y_pred = model.forward(x_data)
+    loss = criterion(y_pred,y_data)
+    print("epoch:", e, "loss:", loss.item())
+    losses.append(loss.item())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+plot_fit('Trained_Model')
+
+plt.plot(range(epochs),losses)
+plt.ylabel("Loss")
+plt.xlabel('epoch')
+plt.show()
+
+print("prediction of point [1,5] is:")
+print(model.predict(torch.Tensor([1,5])))
